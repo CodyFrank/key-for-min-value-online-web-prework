05d3bfddef441444b50260df32d44c26fce308b0
Cody Frank <cody.frank30@gmail.com>
2019-04-02 22:03:24 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < last_value || last_value == 0
       last_value = value
-      lowest_key = key
       puts lowest_key
-    #elsif name_hash == {} 
-      #return nil
     end
   end
 end
