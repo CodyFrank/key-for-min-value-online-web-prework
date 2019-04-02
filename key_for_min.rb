1d28d939dc30ee997fceb17ed608b425c7974329
Cody Frank <cody.frank30@gmail.com>
2019-04-02 21:57:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  last_value = " "
+  name_hash.each do |key, value|
+    if value < last_value || last_value == " "
+      last_value = value
+      lowest_key = key
+      puts lowest_key
+    #elsif name_hash == {} 
+      #return nil
+    end
+  end
 end
